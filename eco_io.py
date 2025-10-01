cb3417e7ae969fa70f34f4c466d1d148d8061cf4
akhtouch.veism <akhtouch.deism24@cordoba.salesianos.edu>
2025-10-01 20:09:01 +0200

    Repetición hasta ejecución correcta

--- eco_io.py
+++ eco_io.py
@@ -1,9 +1,12 @@
-try:
-    num1 = int(input("Introduce el primer número: "))
-    num2 = int(input("Introduce el segundo número: "))
-    num3 = int(input("Introduce el tercer número: "))
-    suma = num1 + num2 + num3
+while True:
+    try:
+        num1 = int(input("Introduce el primer número: "))
+        num2 = int(input("Introduce el segundo número: "))
+        num3 = int(input("Introduce el tercer número: "))
+        suma = num1 + num2 + num3
 
-    print(f"La suma de los 3 números es: {suma}.\nLa media es {suma/3}.\nEl mayor es {max(num1,num2,num3)}.\nEl menor es {min(num1,num2,num3)}.")
-except ValueError:
-    print("Debes introducir un número")
+        print(f"La suma de los 3 números es: {suma}.\nLa media es {suma/3}.\nEl mayor es {max(num1,num2,num3)}.\nEl menor es {min(num1,num2,num3)}.")
+        break
+    except ValueError:
+        print("Debes introducir un número")
+        continue
