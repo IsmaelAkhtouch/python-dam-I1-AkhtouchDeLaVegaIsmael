ca7034edac89a3254e19e0f22ac33c806f70d791
akhtouch.veism <akhtouch.deism24@cordoba.salesianos.edu>
2025-10-01 17:56:07 +0200

    Añadición de eco

---
+++ eco_io.py
@@ -0,0 +1,6 @@
+num1 = int(input("Introduce el primer número: "))
+num2 = int(input("Introduce el segundo número: "))
+num3 = int(input("Introduce el tercer número: "))
+suma = num1 + num2 + num3
+
+print(f"La suma de los 3 números es: {suma}, la media es {suma/3} y el mayor es {max(num1,num2,num3)}")
